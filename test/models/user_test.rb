ffe38d62ab268657a233f81cc760fbfe58eb829d
Luiz Raupp <rauppluizfr@gmail.com>
2016-05-21 09:59:48 -0300

    some validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,16 @@ class UserTest < ActiveSupport::TestCase
     u = User.new
     assert !u.valid?
   end
+
+  test "client_needs_password" do
+    c = Client.new
+    c.email = "teste@teste.com.br"
+    assert !c.valid?
+  end
+
+  test "comman_user_does_not_need_password" do
+    u = User.new
+    u.email = "teste@teste.com.br"
+    assert u.valid?
+  end
 end
