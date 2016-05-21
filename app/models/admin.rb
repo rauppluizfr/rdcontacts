bd98683a078174235f76d27fcc20b3e8785592ae
Luiz Raupp <rauppluizfr@gmail.com>
2016-05-21 10:38:51 -0300

    added model admin;some feedback messages at login page

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,6 @@
-class User < ActiveRecord::Base
+class Admin < User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-private
-  def password_required?
-    self.type == 'Client' || self.type == 'Admin'
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email: "admin@rdcontacts.com", password: "12345678", type: "Admin")
